cc782f976fc291072804731ce36fe81db5206e29
ashutosh Bhawaneedin <ashutoshbhawaneedin@gmail.com>
2024-05-24 14:55:43 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python3 manage.py makemigrations
 python3 manage.py migrate 
 python3 manage.py collectstatic --noinput
 python3 manage.py createMibsuser --first_name admin --last_name admin --username admin --password admin --email admin@example.com --phone 1234567890
-gunicorn --bind mibs5:8000 Mibs.wsgi:application
+gunicorn --bind mibs5:8000 p01--mibs5--wrr87x5dd9qy.code.run
