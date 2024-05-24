adb3239735978ef577b3c791f3a5e3e885c23ad8
ashutosh Bhawaneedin <ashutoshbhawaneedin@gmail.com>
2024-05-24 15:37:52 +0400

    settings.py

--- Mibs/settings.py
+++ Mibs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 env = environ.Env(
-    DEBUG=(bool, True),
+    DEBUG=(bool, False),
     SECRET_KEY=(
         str,
         "django-insecure-j8op9)1q8$1&0^s&p*_0%d#pr@w9qj@1o=3#@d=a(^@9@zd@%j",
